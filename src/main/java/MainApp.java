93075009716d02a06f81bf651891730b0dd46c42
larry <ladams1776@yahoo.com>
2018-08-20 21:06:02 -0400

    Moved components to the Adapter package, created a MailerAdapter to clean up the code in the Main.java.

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,50 +1,28 @@
-import Authentication.SmtpAuthenticator;
-import Logger.InfoLogger;
+import Adapter.InfoLoggerAdapter;
+import Adapter.MailerAdapter;
 import config.UserCredentialConfig;
 
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.util.*;
 
 public class MainApp {
 
     public static void main(String[] args) {
         String subject = "testing now";
         String body = "testing body now";
-        InfoLogger logger = new InfoLogger();
+        InfoLoggerAdapter logger = new InfoLoggerAdapter();
         sendFromGMail(logger, UserCredentialConfig.USERNAME, UserCredentialConfig.PASSWORD, UserCredentialConfig.RECIPIENT, subject, body);
     }
 
-    private static void sendFromGMail(InfoLogger logger, String from, String pass, String to, String subject, String body) {
-        Properties props = System.getProperties();
-        String host = "smtp.gmail.com";
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", from);
-        props.put("mail.smtp.password", pass);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
+    private static void sendFromGMail(InfoLoggerAdapter logger, String from, String pass, String to, String subject, String body) {
 
-        SmtpAuthenticator authenticator = new SmtpAuthenticator(from, pass);
-        Session session = Session.getDefaultInstance(props, authenticator);
 
 
 
         try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            InternetAddress[] toAddress = new InternetAddress[1];
-
-            toAddress[0] = new InternetAddress(to);
-
-            message.addRecipient(Message.RecipientType.TO, toAddress[0]);
-
-            message.setSubject(subject);
-            message.setText(body);
-            Transport transport = session.getTransport("smtp");
-            transport.connect();
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
+            MailerAdapter mailer = new MailerAdapter(from, pass, to);
+            mailer.setSubjectAndBody(subject, body);
+            mailer.sendMessage();
 
         } catch (AddressException ae) {
             logger.info("Address Exception thrown: " + ae.getMessage());
