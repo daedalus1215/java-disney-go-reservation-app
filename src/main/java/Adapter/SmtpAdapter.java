bf1517b7f972059882d2c095d798f62a9d162bf9
larry <ladams1776@yahoo.com>
2018-08-22 07:08:29 -0400

    Adjusted the SmtpAuthenticator to make it a SmtpAdapter so we can include it in the same package

--- src/main/java/Adapter/SmtpAdapter.java
+++ src/main/java/Adapter/SmtpAdapter.java
@@ -1,13 +1,13 @@
-package Authentication;
+package Adapter;
 
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
-public class SmtpAuthenticator extends Authenticator {
+public class SmtpAdapter extends Authenticator {
     private String username;
     private String password;
 
-    public SmtpAuthenticator(String username, String password) {
+    public SmtpAdapter(String username, String password) {
         this.username = username;
         this.password = password;
     }
