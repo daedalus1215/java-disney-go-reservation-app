0e6633b4d7668b6c224d87d0c2dda3266515942b
larry <ladams1776@yahoo.com>
2018-08-22 20:49:31 -0400

    Creating the tests and the first wave of classes.

---
+++ src/main/java/WebPageEssentials/Reference/HtmlElementReferrer.java
@@ -0,0 +1,11 @@
+package WebPageEssentials.Reference;
+
+public final class HtmlElementReferrer {
+    public final String DATE_ID_XPATH                   = "//*[@id=\"diningAvailabilityForm-searchDate\"]";
+    public final String TIME_ID_XPATH                   = "//*[@id=\"diningAvailabilityForm-searchTime\"]";
+    public final String PARTY_SIZE_XPATH                = "//*[@id=\"partySize\"]";
+    public final String SEARCH_TIME_BTN_ID              = "dineAvailSearchButton";
+    public final String DINING_RESERVATION_TITLE_XPATH  = "/html/body/div[1]/div[2]/div[4]/div/div/div[4]/div[2]/span/div[2]/div[4]/div[2]/span[1]";
+
+    public final String DESIRED_TIME_FOR_FIELD_OPTION = "80000714";
+}
