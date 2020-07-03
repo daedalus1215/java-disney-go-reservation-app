2dea8b79e5b0146a803aa2c2a801cd5c72a2499b
larry adams <ladams1776@yahoo.com>
2020-07-03 09:33:19 -0400

    Create HomePage - I am not sure I like this way of testing.
    I can already see that superordinate component's tests will
    constantly have to change. I will have to find a more
    appropriate testing api. It is nice that they
    generate the render output, so... maybe it is not a big deal.

---
+++ disney-reservation-app/src/pages/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReservationEventListView from './ReservationEventListView';
+import AddReservationForm from './AddReservationForm';
+
+const HomePage = () => {
+    return <div>
+        HomePage!
+        <ReservationEventListView />
+        <AddReservationForm />
+    </div>
+};
+
+export default HomePage;
