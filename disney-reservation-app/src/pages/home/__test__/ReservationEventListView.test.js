bc3c11adc32ff310acfe3680e3f62c7030b1870e
larry adams <ladams1776@yahoo.com>
2020-07-03 09:27:18 -0400

    Remove old enzyme style boilerplate Old pattern we don't need anymore

--- disney-reservation-app/src/pages/home/__test__/ReservationEventListView.test.js
+++ disney-reservation-app/src/pages/home/__test__/ReservationEventListView.test.js
@@ -2,20 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ReservationEventListView from "../ReservationEventListView";
 
-describe("src/pages/home/__test__/ReservationEventListView", () => {
-  describe("ReservationEventListView", () => {
-    it("should...", () => {
-      // Arrange
-      const { container } = render(<ReservationEventListView />);
+test("should render ReservationEventListView", () => {
+  // Arrange
+  const { container } = render(<ReservationEventListView />);
 
-      // Act
+  // Act
 
-      // Assert
-      expect(container.firstChild).toMatchInlineSnapshot(`
+  // Assert
+  expect(container.firstChild).toMatchInlineSnapshot(`
         <div>
           This will be a listview
         </div>
       `);
-    });
-  });
 });
