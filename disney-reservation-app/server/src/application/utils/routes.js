dd64060db0deff59bb1d1b67873ac8ce6db18fa6
larry adams <ladams1776@yahoo.com>
2020-07-05 19:38:43 -0400

    Refactor where some of the pieces are located

--- disney-reservation-app/server/src/application/utils/routes.js
+++ disney-reservation-app/server/src/application/utils/routes.js
@@ -1,5 +1,5 @@
 const routes = (app) => {
-    app.get('/status', (req, res) => { res.status(200).end(); });
+    app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
     app.head('/status', (req, res) => { res.status(200).end(); });
 }
 
