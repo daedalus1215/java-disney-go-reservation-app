f9d96b6edbd60caa44f707c5e8903b3a41951e96
larry adams <ladams1776@yahoo.com>
2020-07-08 22:09:46 -0400

    Remove the react app portion of of this project

--- disney-reservation-app/server/src/application/utils/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = (app) => {
-    app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
-    app.head('/status', (req, res) => { res.status(200).end(); });
-}
-
-
-module.exports = routes;
