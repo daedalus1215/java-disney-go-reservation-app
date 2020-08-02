d2f82e7329348be53a0bdf3f4441d99458efbcd8
larry adams <ladams1776@yahoo.com>
2020-08-02 08:12:59 -0400

    GetNextDate now implements BiFunctional Interface

--- src/main/java/disney/reservation/notification/domain/utils/GetNextDate.java
+++ src/main/java/disney/reservation/notification/domain/utils/GetNextDate.java
@@ -2,20 +2,34 @@ package disney.reservation.notification.domain.utils;
 
 import static java.util.Locale.ENGLISH;
 
+import disney.reservation.notification.infrastructure.log.InfoLoggerAdapter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.function.BiFunction;
 
-//@TODO: If we inject logger, we can absorb the exception and make this a Functional Interface class.
-public class GetNextDate {
+public class GetNextDate implements BiFunction<String, Integer, String> {
 
-  public String apply(String date, int interval) throws ParseException {
+  private final InfoLoggerAdapter logger;
+
+  public GetNextDate(InfoLoggerAdapter logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public String apply(String date, Integer interval) {
     final SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", ENGLISH);
-    final Date start = sdf.parse(date);
-    final Calendar calendar = Calendar.getInstance();
-    calendar.setTime(start);
-    calendar.add(Calendar.DAY_OF_YEAR, interval);
-    return sdf.format(calendar.getTime());
+    final Date start;
+    try {
+      start = sdf.parse(date);
+      final Calendar calendar = Calendar.getInstance();
+      calendar.setTime(start);
+      calendar.add(Calendar.DAY_OF_YEAR, interval);
+      return sdf.format(calendar.getTime());
+    } catch (ParseException e) {
+      logger.info("Error with getting next date: ".concat(e.getMessage()));
+      return null;
+    }
   }
 }
--- src/test/java/disney/reservation/notification/domain/utils/GetNextDateTest.java
+++ src/test/java/disney/reservation/notification/domain/utils/GetNextDateTest.java
@@ -2,15 +2,17 @@ package disney.reservation.notification.domain.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import disney.reservation.notification.infrastructure.log.InfoLoggerAdapter;
 import java.text.ParseException;
 import org.junit.jupiter.api.Test;
 
 class GetNextDateTest {
-  private GetNextDate target = new GetNextDate();
+  private GetNextDate target = new GetNextDate(new InfoLoggerAdapter());
 
   @Test
-  public void apply() throws ParseException {
-    final String actual = target.apply("10/11/2020");
+  public void apply_withADateAndIntervalOfOne_willReturnNextDate() throws ParseException {
+    final String actual = target.apply("10/11/2020", 1);
     assertEquals("10/12/2020", actual);
   }
+  //@TODO: Can test the exception throwing.
 }
