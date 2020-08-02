5d88dd677cf175ed299aa84445ca3d9d89c2369a
larry adams <ladams1776@yahoo.com>
2020-08-02 08:22:38 -0400

    Inject locator into the CheckForReservationAndEmail to add one level of decoupling
    This will nicely segway into using Guice over Spring

--- src/main/java/disney/reservation/notification/application/CheckForReservationAndEmail.java
+++ src/main/java/disney/reservation/notification/application/CheckForReservationAndEmail.java
@@ -2,33 +2,35 @@ package disney.reservation.notification.application;
 
 import disney.reservation.notification.domain.WebPageEssentials.Requestor.PageRequestor;
 import disney.reservation.notification.domain.WebPageEssentials.ReservationResolver;
+import disney.reservation.notification.domain.reservations.assemblers.EventAssembler;
+import disney.reservation.notification.domain.reservations.value_objects.Event;
 import disney.reservation.notification.infrastructure.log.InfoLoggerAdapter;
 import disney.reservation.notification.infrastructure.reservations.ReservationRepository;
-import java.util.function.BiFunction;
+import java.util.List;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 final public class CheckForReservationAndEmail {
 
-  final ApplicationContext serviceLocator =
-      new AnnotationConfigApplicationContext(NotificationContext.class);
+  private final ApplicationContext serviceLocator;
+
+  public CheckForReservationAndEmail(ApplicationContext serviceLocator) {
+    this.serviceLocator = serviceLocator;
+  }
 
   public void apply() {
     final InfoLoggerAdapter logger = serviceLocator.getBean(InfoLoggerAdapter.class);
     final PageRequestor pageRequestor = serviceLocator.getBean(PageRequestor.class);
-    final ReservationRepository reservationRepository =
-        serviceLocator.getBean(ReservationRepository.class);
+    final ReservationRepository reservationRepository = serviceLocator
+        .getBean(ReservationRepository.class);
+    final EventAssembler eventAssembler = serviceLocator.getBean(EventAssembler.class);
+    final List<Event> events = eventAssembler.apply(reservationRepository.fetchAll());
 
-    final ReservationResolver reservationResolver =
-        serviceLocator.getBean(ReservationResolver.class);
-
-    final String subject = "testing now";
-    final String body = "testing body now";
-    final String url = "https://disneyworld.disney.go.com/dining/polynesian-resort/ohana/";
+    final ReservationResolver reservationResolver = serviceLocator
+        .getBean(ReservationResolver.class);
 
     try {
       reservationResolver
-          .checkForAvailabilityAndEmail(reservationRepository.fetchAll(), pageRequestor);
+          .checkForAvailabilityAndEmail(events, pageRequestor);
 
     } catch (Exception e) {
       logger.info("Exception thrown: " + e.getMessage());
