e234f487ed8587376ea59bbf781c01746ed7dfe2
larry adams <ladams1776@yahoo.com>
2020-08-03 23:32:23 -0400

    Changed the name of ReservationEvent to Event

--- src/main/java/disney/reservation/notification/domain/WebPageEssentials/Reservation/DataMapper/ReservationDataMapper.java
+++ src/main/java/disney/reservation/notification/domain/WebPageEssentials/Reservation/DataMapper/ReservationDataMapper.java
@@ -1,9 +1,9 @@
 package disney.reservation.notification.domain.WebPageEssentials.Reservation.DataMapper;
 
 import disney.reservation.notification.domain.WebPageEssentials.Reservation.DataMapper.Parser.Exception.ReservationParserException;
-import disney.reservation.notification.domain.WebPageEssentials.Reservation.Entity.ReservationEvent;
+import disney.reservation.notification.domain.WebPageEssentials.Reservation.Entity.Event;
 import java.util.ArrayList;
 
 public interface ReservationDataMapper {
-    public ArrayList<ReservationEvent> load() throws java.text.ParseException, ReservationParserException;
+    public ArrayList<Event> load() throws java.text.ParseException, ReservationParserException;
 }
