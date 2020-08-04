8212bdff862672b0f0a4b68fc69483fcb5b9d5b6
larry adams <ladams1776@yahoo.com>
2020-08-03 23:33:54 -0400

    Misc files that need to change because of ReservationEvent's name change

--- src/test/java/disney/reservation/notification/domain/WebPageEssentials/Reservation/DataMapper/Parser/ReservationDataMapperImplTest.java
+++ src/test/java/disney/reservation/notification/domain/WebPageEssentials/Reservation/DataMapper/Parser/ReservationDataMapperImplTest.java
@@ -4,7 +4,7 @@ package disney.reservation.notification.domain.WebPageEssentials.Reservation.Dat
 import MocksAndStubs.DateDataMapperStubFactory;
 import disney.reservation.notification.domain.WebPageEssentials.Reservation.DataMapper.Parser.Exception.ReservationParserException;
 import disney.reservation.notification.domain.WebPageEssentials.Reservation.DataMapper.ReservationDataMapperImpl;
-import disney.reservation.notification.domain.WebPageEssentials.Reservation.Entity.ReservationEvent;
+import disney.reservation.notification.domain.WebPageEssentials.Reservation.Entity.Event;
 import disney.reservation.notification.domain.WebPageEssentials.Reservation.Entity.ValueObject.DateImpl;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,9 +26,9 @@ public class ReservationDataMapperImplTest {
     @Test
     public void testLoadEquals() throws ParseException, ReservationParserException {
         this.setup();
-        ArrayList<ReservationEvent> reservationEvents = this.tester.load();
-        Assert.assertEquals(new DateImpl("10/12/2013", "10:00", "4").toString(), reservationEvents.get(0).dates.get(0).toString());
-        Assert.assertEquals("testing-event-name", reservationEvents.get(0).name);
-        Assert.assertEquals("http://test.com1", reservationEvents.get(0).url);
+        ArrayList<Event> events = this.tester.load();
+        Assert.assertEquals(new DateImpl("10/12/2013", "10:00", "4").toString(), events.get(0).dates.get(0).toString());
+        Assert.assertEquals("testing-event-name", events.get(0).name);
+        Assert.assertEquals("http://test.com1", events.get(0).url);
     }
 }
