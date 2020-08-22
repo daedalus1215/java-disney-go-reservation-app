a9ae050bef51a361deee252f8e25fdded79ffa09
larry adams <ladams1776@yahoo.com>
2020-08-22 16:33:35 -0400

    Added the logic for the lumber price stuff. Will need to write a test next.

--- src/main/java/disney/reservation/notification/domain/lumber/FetchLumberPriceAndNotify.java
+++ src/main/java/disney/reservation/notification/domain/lumber/FetchLumberPriceAndNotify.java
@@ -3,10 +3,10 @@ package disney.reservation.notification.domain.lumber;
 import static disney.reservation.notification.domain.lumber.HtmlElementReferrer.PRICE_CSS_PATH;
 import static disney.reservation.notification.domain.lumber.HtmlElementReferrer.PRODUCE_TITLE_CSS_PATH;
 
+import disney.reservation.notification.application.lumber.DecimalExpression;
 import disney.reservation.notification.domain.log.Logger;
 import disney.reservation.notification.infrastructure.mail.MailerProxy;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.math.BigDecimal;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -18,10 +18,13 @@ public class FetchLumberPriceAndNotify {
 
   private static final String BASE_URL = "https://www.lumberliquidators.com/ll/c/Select-Brazilian-Pecan-Solid-Hardwood-Flooring-BELLAWOOD-BWBP2SV/10046136";
   private static final int TEN_SECONDS = 10;
+  private static final Double THRESH_HOLD = 3.50;
 
   private final RemoteWebDriver driver;
   private final Logger logger;
   private final MailerProxy mailer;
+
+  private final DecimalExpression decimalExpression = new DecimalExpression();
 
   public FetchLumberPriceAndNotify(RemoteWebDriver driver, Logger logger, MailerProxy mailer) {
     this.driver = driver;
@@ -30,7 +33,7 @@ public class FetchLumberPriceAndNotify {
   }
 
   @Test(groups = {"lumber"})
-  public void apply() {
+  public void apply() throws Exception {
     this.driver.get(BASE_URL);
     this.driver.manage().window().maximize();
 
@@ -38,49 +41,23 @@ public class FetchLumberPriceAndNotify {
 
     webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(PRICE_CSS_PATH)));
 
-    final WebElement price = driver.findElementByCssSelector(PRICE_CSS_PATH);
-    final String priceValue = price.getText();
+    final WebElement priceWidget = driver.findElementByCssSelector(PRICE_CSS_PATH);
+    final String priceWidgetValue = priceWidget.getText();
 
     final WebElement productTitle = driver.findElementByCssSelector(PRODUCE_TITLE_CSS_PATH);
     final String title = productTitle.getText();
 
     logger.info(title
         .concat("\n with price: ")
-        .concat(priceValue));
+        .concat(priceWidgetValue));
 
-    try {
+    final BigDecimal price = decimalExpression.apply(priceWidgetValue);
 
-      final Matcher matcher = Pattern.compile("[0-9].[0-9]+|[0-9]+").matcher(priceValue);
-      while (matcher.find()) {
-        final String group = matcher.group();
-        final String group1 = matcher.group(1);
-        final Integer integer = Integer.getInteger(group);
-        if (integer > Integer.getInteger("5.00")) {
-
-          logger.info("\n below 5.00"
-              .concat(title)
-              .concat("\n")
-              .concat(priceValue)
-          );
-        } else {
-          logger.info("\n greater than 5.00"
-              .concat(title)
-              .concat("\n")
-              .concat(priceValue)
-          );
-
-        }
-      }
-      //mailer.send(title, priceValue);
-    } catch (Exception e) {
-      logger.info("Error sending out a message: "
-          .concat(title)
-          .concat("\n")
-          .concat(priceValue)
-          .concat("\n--")
-          .concat(e.getMessage()));
-    } finally {
-      this.driver.close();
+    if (price.doubleValue() < THRESH_HOLD) {
+      mailer.send(title, priceWidgetValue);
+      logger.info("was less than threshold! Sending out txt message!");
+    } else {
+      logger.info("was more than threshold. Not sending out txt message.");
     }
   }
 }
